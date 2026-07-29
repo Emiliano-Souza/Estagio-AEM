68e065a977a3c23b4bc0da6cea753cf811b74460
emiliano.fs.junior <emiliano.fs.junior@gmail.com>
2026-07-29 18:11:59 -0300

    feat(model): implementa consulta dinâmica do magazine

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/UltimasDoMagazine.java
@@ -0,0 +1,12 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import java.util.List;
+
+public interface UltimasDoMagazine {
+
+    int getQuantidade();
+
+    List<ArtigoMagazine> getArtigos();
+
+    boolean isVazio();
+}
